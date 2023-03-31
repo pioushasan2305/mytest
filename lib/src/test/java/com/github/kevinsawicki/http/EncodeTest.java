f00cc1b64a66fbce9c0d4ee1a5b8ee66f99f00aa
pioushasan2305 <pioushasan2305@gmail.com>
2023-03-31 06:16:54 -0400

    added condition

--- lib/src/test/java/com/github/kevinsawicki/http/EncodeTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/EncodeTest.java
@@ -38,69 +38,30 @@ public class EncodeTest {
   @Test
   public void encode() {
     assertEquals("http://google.com", HttpRequest.encode("http://google.com"));
+    int x=7;
+    String str1="Test";
+    if(x==5)
+    {
+      str1.concat(" Concat");
+    }
+    else {
+      StringBuilder sb = new StringBuilder(str1);
+      str1 = sb.reverse().toString();
+    }
 
-    assertEquals("https://google.com", HttpRequest.encode("https://google.com"));
-
-    assertEquals("http://google.com/a",
-        HttpRequest.encode("http://google.com/a"));
-
-    assertEquals("http://google.com/a/",
-        HttpRequest.encode("http://google.com/a/"));
-
-    assertEquals("http://google.com/a/b",
-        HttpRequest.encode("http://google.com/a/b"));
-
-    assertEquals("http://google.com/a?",
-        HttpRequest.encode("http://google.com/a?"));
-
-    assertEquals("http://google.com/a?b=c",
-        HttpRequest.encode("http://google.com/a?b=c"));
-
-    assertEquals("http://google.com/a?b=c%20d",
-        HttpRequest.encode("http://google.com/a?b=c d"));
-
-    assertEquals("http://google.com/a%20b",
-        HttpRequest.encode("http://google.com/a b"));
-
-    assertEquals("http://google.com/a.b",
-        HttpRequest.encode("http://google.com/a.b"));
-
-    assertEquals("http://google.com/%E2%9C%93?a=b",
-        HttpRequest.encode("http://google.com/\u2713?a=b"));
-
-    assertEquals("http://google.com/a%5Eb",
-        HttpRequest.encode("http://google.com/a^b"));
-
-    assertEquals("http://google.com/%25",
-        HttpRequest.encode("http://google.com/%"));
-
-    assertEquals("http://google.com/a.b?c=d.e",
-        HttpRequest.encode("http://google.com/a.b?c=d.e"));
-
-    assertEquals("http://google.com/a.b?c=d/e",
-        HttpRequest.encode("http://google.com/a.b?c=d/e"));
-
-    assertEquals("http://google.com/a?%E2%98%91",
-        HttpRequest.encode("http://google.com/a?\u2611"));
-
-    assertEquals("http://google.com/a?b=%E2%98%90",
-        HttpRequest.encode("http://google.com/a?b=\u2610"));
-
-    assertEquals("http://google.com/a?b=c%2Bd&e=f%2Bg",
-        HttpRequest.encode("http://google.com/a?b=c+d&e=f+g"));
-
-    assertEquals("http://google.com/+",
-        HttpRequest.encode("http://google.com/+"));
-
-    assertEquals("http://google.com/+?a=b%2Bc",
-        HttpRequest.encode("http://google.com/+?a=b+c"));
   }
-
-  /**
-   * Encoding malformed URI
-   */
-  @Test(expected = HttpRequestException.class)
-  public void encodeMalformedUri() {
-    HttpRequest.encode("\\m/");
+  @Test
+  public void condition() {
+    int x=5;
+    String str1="Test";
+    if(x==5)
+    {
+      str1.concat(" Concat");
+    }
+    else {
+      StringBuilder sb = new StringBuilder(str1);
+      str1 = sb.reverse().toString();
+    }
+    System.out.println(str1);
   }
 }
